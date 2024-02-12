2ac969f4fdba2ae9231d9ddfccfceaa2934947da
Ashwin Sathiyanarayanan <66624673+ash-02@users.noreply.github.com>
2024-02-12 18:56:32 -0500

    made progress on Assignment 3

---
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -0,0 +1,172 @@
+import { Link, useLocation } from "react-router-dom";
+import "./index.css"; // feel free to use the CSS from previous assignments
+function CourseNavigation() {
+    const links = ["Home", "Modules", "Piazza", "Grades", "Assignments"];
+    const { pathname } = useLocation();
+    return (
+        // <ul className="wd-navigation">
+        //     {links.map((link, index) => (
+        //         <li key={index} 
+        //             className={pathname.includes(link) ? "wd-active" : ""}>
+        //             <Link to={link}>{link}</Link>
+        //         </li>
+        //     ))}
+        // </ul>
+        <>
+            <div className="d-none d-md-block ">
+                <ul className="wd-navigation">
+                    {
+                        links.map((link, index) => (
+                            <li key={index}
+                                className={pathname.includes(link) ? "wd-active" : ""}>
+                                <Link to={link}>{link}</Link>
+                            </li>
+                        ))
+                    }
+                    {/* <li><a href="/kanbas/courses/home/screen.html">Home</a></li>
+                    <li className="wd-active"><a href="/kanbas/courses/modules/screen.html">Modules</a></li>
+                    <li><a href="#">Piazza</a></li>
+                    <li><a href="#">Zoom Meetings</a></li>
+                    <li><a href="/kanbas/courses/assignments/index.html">Assignments</a></li>
+                    <li><a href="#">Quizzes</a></li>
+                    <li><a href="/kanbas/courses/grades/screen.html">Grades</a></li>
+                    <li><a href="#">People</a></li>
+                    <li><a href="#">Panopto Video</a></li>
+                    <li><a href="#">Discussions</a></li>
+                    <li><a href="#">Announcements</a></li>
+                    <li><a href="#">Pages</a></li>
+                    <li><a href="#">Files</a></li>
+                    <li><a href="#">Rubrics</a></li>
+                    <li><a href="#">Outcomes</a></li>
+                    <li><a href="#">Collaborations</a></li>
+                    <li><a href="#">Syllabus</a></li>
+                    <li><a href="/kanbas/courses/settings/index.html">Settings</a></li> */}
+                </ul>
+            </div>
+            <div className="course-nav-small-screen d-md-none" style={{
+                height: "100vh"
+            }}>
+                <div className="d-md-none d-flex flex-row justify-content-between align-items-center bg-black text-white px-4">
+                    <a href="/Kanbas/navigation/index.html" style={{
+                        textDecoration: "none",
+                        color: "white"
+                    }}>
+                        <i className="
+                fa fa-bars
+                "></i>
+                    </a>
+                    <div className="p-2">
+                        <h6 className="m-0" style={{
+                            textAlign: "center",
+                        }}>
+                            CS5610.12631.2024
+                            <br />
+                                <span>
+                                    Home
+                                </span>
+                        </h6>
+                    </div>
+                    <div>
+                        <i className="
+                fa fa-caret-down
+                "></i>
+                    </div>
+                </div>
+                <ul className="h-100">
+                    <li><a href="/kanbas/courses/home/screen.html">
+                        <i className="
+                fa fa-home
+                "></i>
+                        Home</a></li>
+                    <li className="wd-active"><a href="/kanbas/courses/modules/screen.html">
+                        <i className="
+                fa fa-th-large
+                "></i>
+                        Modules</a></li>
+                    <li><a href="#">
+                        <i className="
+                fa fa-plug
+                "></i>
+                        Piazza</a></li>
+                    <li><a href="#">
+                        <i className="
+                fa fa-plug
+                "></i>
+                        Zoom Meetings</a></li>
+                    <li><a href="/kanbas/courses/assignments/index.html">
+                        <i className="
+                fa fa-pencil-square-o
+                "></i>
+                        Assignments</a></li>
+                    <li><a href="#">
+                        <i className="
+                fa fa-rocket
+                "></i>
+                        Quizzes</a></li>
+                    <li><a href="/kanbas/courses/grades/screen.html">
+                        <i className="
+                fa fa-book
+                "></i>
+                        Grades</a></li>
+                    <li><a href="#">
+                        <i className="
+                fa fa-users
+                "></i>
+                        People</a></li>
+                    <li><a href="#">
+                        <i className="
+                fa fa-plug
+                "></i>
+                        Panopto Video</a></li>
+                    <li><a href="#">
+                        <i className="
+                fa fa-comments
+                "></i>
+                        Discussions</a></li>
+                    <li><a href="#">
+                        <i className="
+                fa fa-bullhorn
+                "></i>
+                        Announcements</a></li>
+                    <li><a href="#">
+                        <i className="
+                fa fa-file
+                "></i>
+                        Pages</a></li>
+                    <li><a href="#">
+                        <i className="
+                fa fa-folder
+                "></i>
+                        Files</a></li>
+                    <li><a href="#">
+                        <i className="
+                fa fa-list
+                "></i>
+                        Rubrics</a></li>
+                    <li><a href="#">
+                        <i className="
+                fa fa-bullseye
+                "></i>
+                        Outcomes</a></li>
+                    <li><a href="#">
+                        <i className="
+                fa fa-circle-o
+                "></i>
+                        Collaborations</a></li>
+                    <li><a href="#">
+                        <i className="
+                fa fa-book
+                "></i>
+                        Syllabus</a></li>
+                    <li><a href="/kanbas/courses/settings/index.html">
+                        <i className="
+                fa fa-cogs
+                "></i>
+                        Settings</a></li>
+                </ul>
+            </div>
+        </>
+    );
+}
+
+export default CourseNavigation;
---
+++ src/Kanbas/Dashboard/index.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { courses } from "../Database";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FaAccessibleIcon, FaCheckDouble, FaFileArchive, FaFileInvoice, FaFilePdf } from "react-icons/fa";
+
+function Dashboard() {
+    return (
+        <>
+            <div className="p-4">
+                <h1>Dashboard</h1>
+                <hr />
+                <h2>Published Courses ({courses.length})
+                </h2>
+                <hr />
+                <div className="row overflow-auto">
+                    <div className="row row-cols-1 row-cols-md-5 g-4">
+                        {
+                            courses.map((course) => {
+                                return (
+                                    <div className="col" style={{
+                                        width: "300px"
+                                    }}>
+                                        <div className="card">
+                                            <img src={
+                                                `/assets/images/${course.image}`
+                                            } className="card-img-top" style={{
+                                                height: "150px"
+                                            }} />
+                                            <div className="card-body">
+                                                <a className="card-title" href="${course.link}" style={{
+                                                    textDecoration: "none",
+                                                    color: "navy",
+                                                    fontWeight: "bold"
+                                                }}>
+                                                    {course.name}
+                                                </a>
+                                                <p className="card-text">
+                                                    {course.number}
+                                                    <br />
+                                                    {course.startDate} | {course.endDate}
+                                                </p>
+                                                <Link to={`/kanbas/courses/${course._id}`}>
+                                                    <FaFileInvoice />
+                                                </Link>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Dashboard;
