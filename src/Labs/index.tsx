9142ee10f258ea5c1ffe72efe411e44cf875ab8c
Ashwin Sathiyanarayanan <66624673+ash-02@users.noreply.github.com>
2024-02-12 01:10:39 -0500

    Completed Lab 3

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,11 +1,14 @@
 import Assignment3 from "./a3";
+import Nav from "../Nav";
 
 function Labs() {
-    return (
-       <div>
-          {/* <h1>Assignment 3</h1> */}
+   return (
+      <div>
+         {/* <h1>Assignment 3</h1> */}
+         <Nav />
          <Assignment3 />
-       </div>
-    );
- }
- export default Labs;
+      </div>
+   );
+}
+
+export default Labs;
