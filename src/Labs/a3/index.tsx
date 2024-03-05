131c28b323442c86172575ad1dc7a6345b17031b
Ashwin Sathiyanarayanan <66624673+ash-02@users.noreply.github.com>
2024-03-05 11:53:52 -0500

    Completed Lab 4

--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -7,8 +7,11 @@ import Highlight from "./Highlight";
 import Add from "./Add";
 import TodoItem from "./todos/TodoItem";
 import TodoList from "./todos/TodoList";
+import { useSelector } from "react-redux";
+import { LabState } from "../store";
 
 function Assignment3() {
+    const { todos } = useSelector((state: LabState) => state.todosReducer);
     return (
         <div className="container">
             <h1>Assignment 3</h1>
@@ -29,6 +32,13 @@ function Assignment3() {
             <br /><hr /><br />
             <PathParameters />
             <br /><hr /><br />
+            <ul className="list-group">
+                {todos.map((todo) => (
+                    <li className="list-group-item" key={todo.id}>
+                        {todo.title}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,14 +1,26 @@
-import Assignment3 from "./a3";
 import Nav from "../Nav";
+import Assignment3 from "./a3";
+import Assignment4 from "./a4";
+import { Routes, Route, Navigate } from "react-router";
+import store from "./store";
+import { Provider } from "react-redux";
 
 function Labs() {
    return (
-      <div>
-         {/* <h1>Assignment 3</h1> */}
-         <Nav />
-         <Assignment3 />
-      </div>
+      <Provider store={store}>
+         <div>
+            <Nav />
+            <Routes>
+               <Route path="/"
+                  element={<Navigate
+                     to="a3" />} />
+               <Route path="a3"
+                  element={<Assignment3 />} />
+               <Route path="a4"
+                  element={<Assignment4 />} />
+            </Routes>
+         </div>
+      </Provider>
    );
 }
-
 export default Labs;
